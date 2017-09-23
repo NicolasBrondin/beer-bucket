ca954326f667de13b0f17eca612644f9a5233b11
NicolasBrondin <nicolas@brondin.com>
2017-09-23 11:35:12 +0200

    [WALLET] Added wallet page

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ if (Object.hasOwnProperty.call(window, "ActiveXObject") && !window.ActiveXObject
     document.getElementById('ie').style.display = 'block';
 }
 
+function getURLParameter(name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+}
+
 
 var count = 1;
 function add_coin(){
@@ -38,6 +42,13 @@ function show_success(){
     document.getElementById('send').style.display = 'none';
     document.getElementById('validation').style.display = 'block';
     window.location.hash='validation';
+}
+
+function show_wallet(){
+    
+    document.getElementById('wallet').style.display = 'block';
+    document.getElementById('wallet-separator').style.display = 'block';
+    window.location.hash='wallet';
 }
 
 function show_validated(){
@@ -76,8 +87,8 @@ function send_beercoins(){
           }
         }
     
-    //request.open('Post', 'https://beer-bucket-api.herokuapp.com/transactions', true);
-    request.open('Post', 'http://localhost:3000/transactions', true);
+    request.open('Post', 'https://beer-bucket-api.herokuapp.com/transactions', true);
+    //request.open('Post', 'http://localhost:3000/transactions', true);
     //request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     request.send(JSON.stringify(data));
         
@@ -86,6 +97,48 @@ function send_beercoins(){
     }
 }
 
+function get_wallet(){
+    show_wallet();
+    var wallet_token = getURLParameter('wallet');
+    if(wallet_token){
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+          if(request.readyState === 4) {
+            if(request.status === 200) { 
+                console.log(request.response);
+                var data = JSON.parse(request.response);
+                console.log(data);
+                document.getElementById('wallet-sum').innerHTML = data.transactions.reduce(function(s,t){
+                    s += t.count;
+                    var p = document.createElement('p');
+                    p.innerHTML = t.count+" received from "+t.email_from;
+                    var p2 = document.createElement('p');
+                    p2.innerHTML = t.beercoins.reduce(function(ss,b){
+                        ss += "# "+b+" #";
+                        return ss; 
+                    },"");
+                    document.getElementById('transaction-list').appendChild(p);
+                    document.getElementById('transaction-list').appendChild(p2);
+                    return s;
+                },0);
+            } else {
+                console.error('An error occurred during your request: ', request.status + ' ' + request.statusText);
+                wallet_error.innerHTML = 'Server error, try again...';
+            } 
+          }
+        }
+    request.open('Get', 'https://beer-bucket-api.herokuapp.com/users?wallet='+wallet_token, true);
+    //request.open('Post', 'http://localhost:3000/transactions', true);
+    //request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.send();
+        
+    } else {
+        wallet_error.innerHTML = 'Wallet not found !';
+    }
+}
+
 if(window.location.href.indexOf('token') > -1){
     show_validated();
+} else if(window.location.href.indexOf('wallet') > -1) {
+    get_wallet();
 }
